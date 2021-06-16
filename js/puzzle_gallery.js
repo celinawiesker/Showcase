3c9cbdca10f322a4ad1c9fb8a825c81aa461d00d
Celina <68855981+celinawiesker@users.noreply.github.com>
2021-06-16 13:58:25 +0200

    16/6 2.1

--- js/puzzle_gallery.js
+++ js/puzzle_gallery.js
@@ -14,7 +14,9 @@ var PuzzleGallery = new Phaser.Class({
     //this.background = this.add.image(0,0,'play_bg')
     //background.setScale(3)
 
-    settingsbtn = this.add.image(10, 20, 'settings', this.gotoMenu, this);
+    let settingsbtn = this.add.image(10, 20, 'settings');
+    settingsbtn.setInteractive();
+    settingsbtn.on('pointerdown', () => this.scene.start('Puzzle_Menu'))
     //settingsbtn.setScale(0.5);
 
     //==============================================================================================
@@ -30,24 +32,28 @@ var PuzzleGallery = new Phaser.Class({
     for (var i = 1; i < 10; i++) {
       count += 1;
       var b = this.add.image(x, y, i.toString());
-      // b.gallery = i.toString();
-      //b.anchor.setTo(0.5);
+
+     // b.gallery = i.toString();
+      b.setOrigin(0.5);
       b.scale.x = 0.3;
       b.scale.y = 0.3;
+      b.on('pointerdown', () => this.scene.start('Puzzle_Play'))
       x += 300;
       // y += 20;
+
       if (count === 3) {
         count = 0;
         y += 220;
         x = 220;
       }
     }
+
     console.log('End Gallery')
   },
 
-  gotoMenu() {
-    this.scene.start('Puzzle_Menu');
-  },
+  // gotoMenu() {
+  //   this.scene.start('Puzzle_Menu');
+  // },
 
   makeBox(x, y) {
     var bmd = this.game.add.bitmapData(x, y);
